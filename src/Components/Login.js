3b13d0a1f730b4dbb80b6ed2ca47796841294902
Muhammad Ali <ali.mis@stmu.edu.pk>
2024-09-12 16:29:28 +0500

    Sun icon seprate

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,31 +1,9 @@
+// src/pages/LoginPage.js
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import "../Styles/LoginPage.css"; // Import the CSS file
-
-const SunIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="sun-icon"
-    style={{ width: "100%", height: "100%" }}
-  >
-    <circle cx="12" cy="12" r="5" />
-    <line x1="12" y1="1" x2="12" y2="3" />
-    <line x1="12" y1="21" x2="12" y2="23" />
-    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-    <line x1="1" y1="12" x2="3" y2="12" />
-    <line x1="21" y1="12" x2="23" y2="12" />
-    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-  </svg>
-);
+import SunIcon from "./assets/SunIcon";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
