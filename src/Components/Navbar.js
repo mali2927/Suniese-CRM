13c251c377f8248a78d8cee29d94dc3a361f02c9
Muhammad Ali <ali.mis@stmu.edu.pk>
2024-09-12 16:25:45 +0500

    logot route

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Sun } from "lucide-react";
 import "../Styles/Navbar.css";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Add login logic here
+    navigate("/"); // Redirect to the dashboard after login
+  };
   return (
     <div className="navbar">
       <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
         <Sun className="navbar-icon" />
         <h1 className="navbar-title">Solar Dashboard</h1>
       </div>
-      <button className="navbar-button">Logout</button>
+      <button className="navbar-button" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
