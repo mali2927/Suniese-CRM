08b92fa4e71ae55c73bd46febedaa9c28b0106cb
mabdullahkh <mak30597@gmail.com>
2024-09-16 13:46:10 +0100

    login backend

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'api_token'
     ];
 
     /**
@@ -30,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'api_token'
     ];
 
     /**
