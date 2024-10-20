211f02cdc9004504fefb919ea721d014d978677a
mali2927 <mali2927@gmail.com>
2024-10-21 00:12:51 +0500

    local config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+// const config = {
+//     baseURL: 'http://18.175.153.218:8000/api',
+//   };
+
 const config = {
-    baseURL: 'http://18.175.153.218:8000/api',
-  };
-  
-  export default config;
+  baseURL: "http://localhost:8000/api",
+};
+export default config;
