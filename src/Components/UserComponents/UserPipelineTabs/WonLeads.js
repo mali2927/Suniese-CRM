c92035b663520233b6f5ee81ac3bc1c0843a882c
mali2927 <mali2927@gmail.com>
2024-11-06 12:17:23 +0500

    date won in user

--- src/Components/UserComponents/UserPipelineTabs/WonLeads.js
+++ src/Components/UserComponents/UserPipelineTabs/WonLeads.js
@@ -86,6 +86,7 @@ const WonLeads = ({
             <th>Phone Number</th>
             <th>Quoted Price</th>
             <th>Total Value</th>
+            <th>Date Won</th>
           </tr>
         </thead>
         <tbody>
@@ -98,6 +99,7 @@ const WonLeads = ({
                 <td>{lead.phone_number}</td>
                 <td>{lead.quoted_price}</td>
                 <td>{lead.total_payment}</td>
+                <td>{new Date(lead.updated_at).toISOString().split("T")[0]}</td>
               </tr>
             ))
           ) : (
